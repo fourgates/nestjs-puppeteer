b286e6e107e1c2c04ed661ec21678c19e7ea170b
phillip.ninan <pninan@lbisoftware.com>
2024-02-14 17:20:26 -0500

    feat: print url or static content

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,7 +12,18 @@ export class AppController {
   }
   @Get('print')
   async pringMe(@Res() res) {
-    const pdf = await this.printPDF();
+    const content = `
+    <!DOCTYPE html>
+    <html>
+    <body>
+    FINDME!!!
+    <h1>My First Heading</h1>
+    <p>My first paragraph.</p>
+    
+    </body>
+    </html>    
+    `;
+    const pdf = await this.printPDFForStaticContent(content);
 
     res.set({
       // pdf
@@ -29,20 +40,23 @@ export class AppController {
     res.end(pdf);
   }
 
-  async printPDF() {
+  async printPDFForUrl(url: string) {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-    const content = `
-    <!DOCTYPE html>
-    <html>
-    <body>
-    FINDME!!!
-    <h1>My First Heading</h1>
-    <p>My first paragraph.</p>
-    
-    </body>
-    </html>    
-    `;
+    await page.goto(url, {
+      waitUntil: 'networkidle0',
+    });
+    const pdf = await page.pdf({ format: 'A4' });
+
+    await browser.close();
+    return pdf;
+  }
+  async printPDFForStaticContent(content: string) {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox'],
+    });
+    const page = await browser.newPage();
 
     await page.setContent(content);
     const pdf = await page.pdf({ format: 'A4' });
