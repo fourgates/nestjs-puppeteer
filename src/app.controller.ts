00704b2ca9a64790a3a2854f5cdd3b51f4bbad2c
PhillipNinan <ninan.phillip@gmail.com>
2024-02-15 23:40:33 -0500

    feat: demo + form endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Res } from '@nestjs/common';
-import puppeteer from 'puppeteer';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import puppeteer, { PDFOptions } from 'puppeteer';
 import { AppService } from './app.service';
 
+interface PrintForm {
+  html: string;
+  filename: string;
+  options: PDFOptions;
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -10,8 +15,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  @Get('print')
-  async pringMe(@Res() res) {
+  @Get('print/test')
+  async printMeTest(@Res() res) {
     const content = `
     <!DOCTYPE html>
     <html>
@@ -23,24 +28,24 @@ export class AppController {
     </body>
     </html>    
     `;
-    const pdf = await this.printPDFForStaticContent(content);
+    const pdf = await this.printPDFForStaticContent(content, { format: 'A4' });
 
-    res.set({
-      // pdf
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': 'attachment; filename=invoice.pdf',
-      'Content-Length': pdf.length,
-
-      // prevent cache
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
-      Pragma: 'no-cache',
-      Expires: 0,
-    });
+    res.set(this.getResponseHeaders('demo.pdf', pdf.length));
 
     res.end(pdf);
   }
 
-  async printPDFForUrl(url: string) {
+  @Post('print')
+  async printMe(@Body() form: PrintForm, @Res() res) {
+    const pdf = await this.printPDFForStaticContent(form.html, form.options);
+
+    res.set(this.getResponseHeaders(form.filename, pdf.length));
+
+    res.status(HttpStatus.OK).end(pdf);
+    //res.status(HttpStatus.OK).send(ingredients);
+  }
+
+  private async printPDFForUrl(url: string) {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto(url, {
@@ -51,17 +56,34 @@ export class AppController {
     await browser.close();
     return pdf;
   }
-  async printPDFForStaticContent(content: string) {
+  private async printPDFForStaticContent(content: string, options: PDFOptions) {
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox'],
+      // may ne needed for non-headless mac
+      //executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+      args: ['--no-sandbox'], // no sandbox needed for docker / linux
     });
     const page = await browser.newPage();
 
     await page.setContent(content);
-    const pdf = await page.pdf({ format: 'A4' });
+    const pdf = await page.pdf(options);
 
     await browser.close();
     return pdf;
+  }
+
+  private getLaunchConfigForMac() {}
+  private getResponseHeaders(filename: string, contentLength: number) {
+    return {
+      // pdf
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': `'attachment; filename=${filename}.pdf'`,
+      'Content-Length': contentLength,
+
+      // prevent cache
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: 0,
+    };
   }
 }
